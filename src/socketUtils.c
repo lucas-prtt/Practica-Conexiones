07266fbcf48821c84bb778da73442258417b3d94
lucas-prtt <lpresotto@frba-utn-edu.ar>
2025-04-11 22:43:49 +0000

    paquetes?

--- src/socketUtils.c
+++ src/socketUtils.c
@@ -52,3 +52,81 @@ int esperarClientes(int socket_server, void (*atenderCliente)(void*)){ //Esperar
     }
 }
 
+void enviarStream(int socket, void* stream, int size){
+    send(socket, stream, size, 0);
+    return;
+}
+void agregarAStream(void * stream, int * offset, void * content, int size){
+    memcpy(stream + offset, content, size);
+    offset += size;
+    return;
+}
+paquete * crearPaquete(){
+    paquete * pac = malloc(sizeof(paquete));
+    pac->siguiente_segmento = NULL;
+    pac->size = -1;
+    return pac;
+}
+void agregarAPaquete(paquete * paquete, void * elementoParaAgregar, int size){
+    paquete * segmento = paquete;
+    while(segmento->siguiente_segmento != NULL){
+        segmento = segmento->siguiente_segmento;
+    }
+    if (segmento->tamanio != -1){
+    segmento->siguiente_segmento = crearPaquete();
+    segmento = segmento->siguiente_segmento;
+    }
+    segmento->size = size;
+    segmento->stream = malloc(size);
+    memcpy(segmento->stream, elementoParaAgregar, size);
+    return;
+    // Estructura dinamica paquete (size, stream, sig). Stream es un puntero a donde se guarde cualquier dato, size el tamaño de ese dato y siguiente_segmento el puntero a la siguiente parte del paquete
+}
+
+int paqueteSizeEntero(paquete * pac){
+    if(pac == NULL)
+    return 0; 
+    tam = 0;
+    while(pac->siguiente_segmento != NULL){
+        tam += pac->size;
+        pac = pac->siguiente_segmento;
+    }
+    tam += pac->size;
+    return tam;
+}
+
+void paqueteAStream(paquete * pac, void * stream){
+    if (pac == NULL){
+        stream = NULL;   
+        return 0;
+    }
+    int tam = paqueteSizeEntero(pac)
+    stream = malloc(tam);
+    int offset = 0;
+    while (pac->siguiente_segmento != NULL){
+        agregarAStream(stream, &offset, pac->stream, pac->size);
+        pac = pac->siguiente_segmento;
+    }
+    agregarAStream(stream, &offset, pac->stream, pac->size);
+    return tam;
+}
+void eliminarPaquete(paquete * pac){
+    paquete * temp;
+    while(pac->siguiente_segmento != NULL){
+        temp = pac;
+        pac = pac->siguiente_segmento;
+        free(temp->stream);
+        free(temp);
+    }
+    free(pac -> stream);
+    free(pac);
+}
+
+
+void enviar_paquete(int socket, paquete * pac)// Envia el paquete en formato int, stream de tamaño int, int, stream de tamaño int, siendo stream cualquier cosa. 
+{
+    void * stream ;
+    int sizeStream = paqueteAStream(pac, stream);
+    enviarStream(socket, stream, sizeStream); 
+    free(stream);
+}
--- src/socketUtils.h
+++ src/socketUtils.h
@@ -4,10 +4,30 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
+
+typedef struct{
+    int size;
+    void * stream;
+    paquete * siguiente_segmento;
+} paquete;
+
 void say(char[] msg);
+
 int crearSocket(char* ip, char* puerto, struct addrinfo *servinfo);
 int crearSocketServer(char* puerto);
 int conectarSocketClient(char* ip, char* puerto);
+
+int esperarClientes(int socket_server, void (*atenderCliente)(void*));
+
+void enviar_paquete(int socket, paquete * pac);
+void eliminarPaquete(paquete * pac);
+void agregarAPaquete(paquete * paquete, void * elementoParaAgregar, int size);
+paquete * crearPaquete();
+
+
+
+
+
 
 #endif
 
